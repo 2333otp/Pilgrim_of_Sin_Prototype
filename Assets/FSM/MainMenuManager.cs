4128f5c06cc01cd81de77704578110cbdd493d35
huvjnk <wuchengyan0510@gmail.com>
2026-03-27 18:01:45 +0800

    場景跳轉成功
    載入介面無法顯示、載入時主畫面不會關閉

--- Assets/FSM/MainMenuManager.cs
+++ Assets/FSM/MainMenuManager.cs
@@ -9,15 +9,15 @@ using UnityEngine.UI;
 public class MainMenuManager : MonoBehaviour
 {
     // private 代表只能在此類別內存取並且隱藏
-    [SerializeField] private Button btnLoad; //繼續遊戲按鈕
+    //[SerializeField] private Button btnLoad; //繼續遊戲按鈕
     [SerializeField] private Button btnNew; //開始遊戲按鈕
-    [SerializeField] private Button btnOptions; //選項按鈕
-    [SerializeField] private Button btnCredits; //製作團隊按鈕
-    [SerializeField] private Button btnQuit; //退出按鈕
-    [SerializeField] private Button btnBackOption; //選項返回按鈕
-    [SerializeField] private Button btnBackCredits; //製作團隊返回按鈕
+    //[SerializeField] private Button btnOptions; //選項按鈕
+    //[SerializeField] private Button btnCredits; //製作團隊按鈕
+    //[SerializeField] private Button btnQuit; //退出按鈕
+    //[SerializeField] private Button btnBackOption; //選項返回按鈕
+   // [SerializeField] private Button btnBackCredits; //製作團隊返回按鈕
     [SerializeField] private CanvasGroup groupOption; //主選單畫布群組
-    [SerializeField] private CanvasGroup groupCredits; //製作團隊畫布群組
+   // [SerializeField] private CanvasGroup groupCredits; //製作團隊畫布群組
 
     private void Awake()
     {
@@ -26,17 +26,17 @@ public class MainMenuManager : MonoBehaviour
         //使用 Lambda 表達式來調用對應的方法
         //StartCoroutine 用於啟動協程
         //控制介面的淡入與淡出
-        btnOptions.onClick.AddListener(() => StartCoroutine(FadeSystem.Fade(groupOption, interval: 0.05f)));
-        btnBackOption.onClick.AddListener(() => StartCoroutine(FadeSystem.Fade(groupOption, false)));
-        btnCredits.onClick.AddListener(() => StartCoroutine(FadeSystem.Fade(groupCredits, interval: 0.05f)));
-        btnBackCredits.onClick.AddListener(() => StartCoroutine(FadeSystem.Fade(groupCredits, false)));
+       // btnOptions.onClick.AddListener(() => StartCoroutine(FadeSystem.Fade(groupOption, interval: 0.05f)));
+       // btnBackOption.onClick.AddListener(() => StartCoroutine(FadeSystem.Fade(groupOption, false)));
+       // btnCredits.onClick.AddListener(() => StartCoroutine(FadeSystem.Fade(groupCredits, interval: 0.05f)));
+       // btnBackCredits.onClick.AddListener(() => StartCoroutine(FadeSystem.Fade(groupCredits, false)));
         //點擊退出按鈕時，退出應用程式
         //Application.Quit() 會在編輯器中停止播放，在打包後的應用程式中退出
-        btnQuit.onClick.AddListener(() =>
-        {
-            Application.Quit();
-            Debug.Log("<color=#ff3>退出遊戲</color>"); //在編輯器中顯示退出訊息
-        });
+       // btnQuit.onClick.AddListener(() =>
+       // {
+          //  Application.Quit();
+           // Debug.Log("<color=#ff3>退出遊戲</color>"); //在編輯器中顯示退出訊息
+       // };
         //點擊開始遊戲按鈕時，載入遊戲場景
         btnNew.onClick.AddListener(() => SceneLoader.instance.LoadSceneAsync("貪的關卡" ));
     }
